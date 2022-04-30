6d5aefbef0825b8f0d73e6cc9ce574e3f4b7f519
chikaberry <79671470+chikaberry@users.noreply.github.com>
2022-04-30 09:37:44 -0600

    personal assignment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const routes = require ('express').Router();
+// const routes = require ('express').Router();
 
-routes.get('/', (req, res) => {
-    res.send('Olongo Wonodi')
-  })
-  module.exports = routes;
+// routes.get('/', (req, res) => {
+//     res.send('Olongo Wonodi')
+//   })
+//   module.exports = routes;
 
+const express = require('express');
+const router = express.Router();
 
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
   // install package to not restart server => npm i --save-dev nodemon
